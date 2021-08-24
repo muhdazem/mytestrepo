009d8557f1b9707546098c12b4bfa66ed529f6bb
muhdazem <azem.azem97@gmail.com>
2021-08-24 14:52:22 +0800

    Finished working with API

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+let url = `https://restcountries.eu/rest/v2/name/Malaysia`;
+
+function waitForData(res) {
+  return res.json();
+}
+
+function handleData(data) {
+  let pop = data[0].population;
+  console.log(`The population of Malaysia is ${pop}`);
+}
+
+fetch(url, { method: "GET" }).then(waitForData).then(handleData);
